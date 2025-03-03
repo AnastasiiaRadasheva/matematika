f3ef1d1def5b392d037d38131b50d881e7438d4d
opilane <opilane@HP-CZC2349HTC>
2025-03-03 09:15:41 +0200

    fdfd

--- matematikanew.py
+++ matematikanew.py
@@ -1,12 +1,12 @@
-
-from random import *
+from random import randint
 
 bebe = int(input('Sisesta tase (1, 2, 3): '))
-tasadejctvia = randint(1, 2)
-tasavariant = randint(1, 4)
-tasavariant3 = randint(1, 2)
+tasadejctvia = randint(1, 2)  
+tasavariant = randint(1, 4)   
+tasavariant3 = randint(1, 2)  
 total_questions = 0
 correct_answers = 0
+
 while True:
     if bebe < 1 or bebe > 3:
         print('Ne pravilnoe chislo')
@@ -24,15 +24,11 @@ while True:
                 vastustase1 = int(input(f'{num} - {num1} = ? '))
                 if taseotv0 == vastustase1:
                     correct_answers += 1  
-                    print('--------------------------------------')
                     print('Molodets!, napishi 666 dlja zakritija')
-                    print('--------------------------------------')
                 elif vastustase1 == 666:
                     break
                 else:
-                    print('--------------------------------------')
                     print('Ne pravilno, napishi 666 dlja zakritija')
-                    print('--------------------------------------')
             except ValueError:
                 print("Pojaluysta, vvedite chislo.")
 
@@ -42,74 +38,69 @@ while True:
                 vastustase1 = int(input(f'{num} + {num1} = ? '))
                 if taseotv0 == vastustase1:
                     correct_answers += 1
-                    print('--------------------------------------')
                     print('Molodets!, napishi 666 dlja zakritija')
-                    print('--------------------------------------')
                 elif vastustase1 == 666:
                     break
                 else:
-                    print('--------------------------------------')
                     print('Ne pravilno, napishi 666 dlja zakritija')
-                    print('--------------------------------------')
             except ValueError:
                 print("Pojaluysta, vvedite chislo.")
+
     elif bebe == 2:
         num2 = randint(1, 20)
         num3 = randint(1, 20)
+
         if tasavariant == 1:
             taseotv1 = num2 - num3
+            operator = "-"
         elif tasavariant == 2:
             taseotv1 = num2 + num3
+            operator = "+"
         elif tasavariant == 3:
             taseotv1 = num2 * num3
+            operator = "*"
         elif tasavariant == 4:
             if num3 == 0:
-                print('--------------------------------------')
                 print('Delit na nol\' nelya!, napishi 666 dlja zakritija')
-                print('--------------------------------------')
                 continue
             taseotv1 = num2 / num3
+            operator = "/"
+
         try:
-            vastustase2 = float(input(f'{num2} {["-", "+", "*", "/"][tasavariant - 1]} {num3} = ? '))
+            vastustase2 = float(input(f'{num2} {operator} {num3} = ? '))
             if round(taseotv1, 2) == round(vastustase2, 2):
                 correct_answers += 1
-                print('--------------------------------------')
                 print('Molodets!, napishi 666 dlja zakritija')
-                print('--------------------------------------')
             elif vastustase2 == 666:
                 break
             else:
-                print('--------------------------------------')
                 print('Ne pravilno, napishi 666 dlja zakritija')
-                print('--------------------------------------')
         except ValueError:
             print("Pojaluysta, vvedite chislo.")
+
     elif bebe == 3:
         num2 = randint(1, 100)
         num3 = randint(1, 100)
 
         if tasavariant3 == 1:
             taseotv1 = num2 * num3
+            operator = "*"
         elif tasavariant3 == 2:
             if num3 == 0:
-                print('--------------------------------------')
                 print('Delit na nol\' nelya!, napishi 666 dlja zakritija')
-                print('--------------------------------------')
                 continue
             taseotv1 = num2 / num3
+            operator = "/"
+
         try:
-            vastustase2 = float(input(f'{num2} {["*", "/"][tasavariant3 - 1]} {num3} = ? '))
+            vastustase2 = float(input(f'{num2} {operator} {num3} = ? '))
             if round(taseotv1, 2) == round(vastustase2, 2):
                 correct_answers += 1
-                print('--------------------------------------')
                 print('Molodets! napishi 666 dlja zakritija')
-                print('--------------------------------------')
             elif vastustase2 == 666:
                 break
             else:
-                print('--------------------------------------')
                 print('Ne pravilno napishi 666 dlja zakritija')
-                print('--------------------------------------')
         except ValueError:
             print("Pojaluysta, vvedite chislo.")
 
